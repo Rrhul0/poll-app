e250851c183acd6414ebfd15b6d29966bd19aea4
Rrhul0 <rr025765@gmail.com>
2023-05-27 23:08:47 +0530

    added logic to add more then 2 options to vote

--- src/components/createPoll.tsx
+++ src/components/createPoll.tsx
@@ -1,40 +1,48 @@
 import { addDoc, serverTimestamp, documentId } from 'firebase/firestore'
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import { pollsDB } from '../lib/firebase'
 
 export default function CreatePoll() {
+    const [options, setOptions] = useState<number>(2)
+
     function onSubmitForm(e: FormEvent<HTMLFormElement>) {
         e.preventDefault()
         const formData = new FormData(e.currentTarget)
+        let optionsData = []
+        for (let index = 0; index < options; index++) {
+            optionsData.push(formData.get('option' + index))
+        }
         const data = {
             name: formData.get('poll_name'),
             desc: formData.get('poll_desc'),
-            options: [formData.get('option1'), formData.get('option2')],
+            options: optionsData,
             timestamp: serverTimestamp(),
         }
         addDoc(pollsDB, data)
     }
+
     return (
-        <>
-            <form onSubmit={onSubmitForm} className='flex flex-col gap-2'>
-                <label>
-                    <span>Poll Name: </span>
-                    <input name='poll_name' placeholder='Name' />
-                </label>
-                <label className='flex flex-col'>
-                    <div>Poll Description</div>
-                    <textarea name='poll_desc' />
-                </label>
-                <label>
-                    <span>Option 1: </span>
-                    <input name='option1' />
-                </label>
-                <label>
-                    <span>Option 2: </span>
-                    <input name='option2' />
-                </label>
-                <button>Create</button>
-            </form>
-        </>
+        <form onSubmit={onSubmitForm} className='flex flex-col gap-2'>
+            <label>
+                <span>Poll Name: </span>
+                <input name='poll_name' placeholder='Name' />
+            </label>
+            <label className='flex flex-col'>
+                <div>Poll Description</div>
+                <textarea name='poll_desc' />
+            </label>
+            {Array(options)
+                .fill(0)
+                .map((_, index) => (
+                    <label key={index}>
+                        <span>Option {index + 1}: </span>
+                        <input name={'option' + index} />
+                    </label>
+                ))}
+            <button type='button' onClick={() => setOptions(o => (o += 1))}>
+                Add more options
+            </button>
+            <button>Create</button>
+        </form>
     )
 }
