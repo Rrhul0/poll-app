d3108b6375abee2455f90f5315c12abea25225f0
Rrhul0 <rr025765@gmail.com>
2023-05-28 20:57:48 +0530

    added option to add more options after poll created and its logic

---
+++ src/components/addMoreOptions.tsx
@@ -0,0 +1,49 @@
+import { updateDoc, doc } from 'firebase/firestore'
+import { FormEvent, useState } from 'react'
+import { db } from '../lib/firebase'
+import { TypePoll } from './polls'
+
+export default function AddMoreOptions({ poll }: { poll: TypePoll }) {
+    const [open, setOpen] = useState(false)
+
+    function onSubmitOptions(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        const newOption = new FormData(e.currentTarget).get('option')
+        const docu = doc(db, 'polls', poll.id)
+        updateDoc(docu, { options: [...poll.options, newOption] })
+    }
+
+    return (
+        <>
+            <button onClick={() => setOpen(true)}>Add more options</button>
+            {open ? (
+                <div className='absolute inset-0 z-10 grid place-content-center bg-slate-100 bg-opacity-50 backdrop-blur-sm'>
+                    <form
+                        onSubmit={onSubmitOptions}
+                        className={
+                            'flex flex-col items-center gap-2 rounded-2xl bg-slate-300 bg-opacity-30 px-20 py-12 backdrop-blur-sm'
+                        }
+                    >
+                        <label>
+                            <div>New Option</div>
+                            <input name='option' />
+                        </label>
+                        <button className='rounded-xl bg-purple-500 px-3 py-1 text-slate-100 hover:bg-purple-600'>
+                            Add option
+                        </button>
+                        <button
+                            type='button'
+                            className='rounded-xl border border-red-500 px-3 py-0.5 text-red-500 hover:bg-red-500 hover:text-white'
+                            onClick={() => setOpen(false)}
+                        >
+                            Cancel
+                        </button>
+                    </form>
+                </div>
+            ) : (
+                ''
+            )}
+        </>
+    )
+}
--- src/components/poll.tsx
+++ src/components/poll.tsx
@@ -1,10 +1,11 @@
 import { onValue, ref, set } from 'firebase/database'
 import { useEffect, useState } from 'react'
 import { realtimeVotesDB } from '../lib/firebase'
+import AddMoreOptions from './addMoreOptions'
 import { TypePoll } from './polls'
 
 export default function Poll({ poll }: { poll: TypePoll }) {
-    const [votes, setVotes] = useState<number[]>(Array(poll.options.length).fill(0))
+    const [votes, setVotes] = useState<number[]>([])
 
     const refDB = ref(realtimeVotesDB, 'poll-' + poll.id)
 
@@ -17,27 +18,28 @@ export default function Poll({ poll }: { poll: TypePoll }) {
         return () => unsub()
     }, [])
 
-    function onClickVote(optionIndex: number) {
+    function onClickVote(optionVotes: number, optionIndex: number) {
         const newRef = ref(realtimeVotesDB, 'poll-' + poll.id + '/' + optionIndex)
-        set(newRef, votes[optionIndex] + 1)
+        set(newRef, optionVotes + 1)
     }
 
     const totalVotes = votes.reduce((pv, cv) => pv + cv, 0) || 1
 
     return (
-        <li className='flex flex-grow flex-col items-center gap-2 rounded-xl bg-white px-4 py-6 drop-shadow-lg '>
+        <li className='relative flex flex-grow flex-col items-center gap-2 rounded-xl bg-white px-4 py-6 drop-shadow-lg '>
             <div>
                 <h2 className='text-center text-xl font-bold capitalize'>{poll.name}</h2>
                 <p className='text-lg capitalize'>{poll.desc}</p>
             </div>
             <div className='flex flex-wrap justify-center gap-2'>
                 {poll.options.map((option, index) => {
-                    const percent = (votes[index] / totalVotes) * 100
+                    const optionVotes = votes[index] || 0
+                    const percent = (optionVotes / totalVotes) * 100 || 0
                     return (
                         <button
                             key={index}
                             className='relative flex aspect-square w-40 flex-col items-center gap-2 overflow-hidden rounded-lg border px-2 py-3'
-                            onClick={() => onClickVote(index)}
+                            onClick={() => onClickVote(optionVotes, index)}
                         >
                             <div
                                 className='absolute bottom-0 left-0 right-0 -z-10 bg-red-300 bg-opacity-60 blur-[2px] duration-300'
@@ -45,11 +47,12 @@ export default function Poll({ poll }: { poll: TypePoll }) {
                             />
                             <div className='capitalize'>{option}</div>
                             <div>{percent.toFixed(2)}%</div>
-                            <div>{votes[index]} votes</div>
+                            <div>{optionVotes} votes</div>
                         </button>
                     )
                 })}
             </div>
+            <AddMoreOptions poll={poll} />
         </li>
     )
 }
